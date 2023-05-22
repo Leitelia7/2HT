2ce654e34da93e319f99dbd2916a760a0475a486
Leitelia7 <134284637+Leitelia7@users.noreply.github.com>
2023-05-22 10:54:21 -0300

    Add files via upload

---
+++ atv lista 1 questa3.py
@@ -0,0 +1,51 @@
+print("=========================================\n")
+print("Seja bem vindo a hamburgueria do Ítalo Diniz")
+print("\n=========================================")
+print("           | Menu |\n")
+print("|Comida|   |Código|   |Valor|  ")
+print("Cachorro-quente = 100 = R$1,20\n")
+print("Bauru = 101 = R$1,30\n")
+print("Hambúrguer = 102 = R$1,20\n")
+print("Cheeseburguer = 103 = R$1,30\n")
+print("Refrigerante = 104 = R$1,00\n")
+pedido = int(input("Qual é seu pedido? --> "))
+if pedido == 100:
+    print("\nDeu o valor de R$1,20")
+elif pedido == 101:
+    print("\nDeu o valor de R$1,30")
+elif pedido == 102:
+    print("\nDeu o valor de R$1,20")
+elif pedido == 103:
+    print("\nDeu o valor de R$1,30")
+elif pedido == 104:
+    print("\nDeu o valor de R$1,00")
+else:
+    print("\nEste código não está registrado na tabela")
+1.	Faça um programa em Python que leia um número inteiro de 1 a 7 e informe o dia da semana correspondente, sendo domingo o dia de número Se o número não corresponder a um dia da semana, mostre uma mensagem de erro.
+dia = int(input("Coloque um número de 1 a 7 para achar o dia da semana: --> "))
+if dia == 1:
+  print("Domingo")
+print("=========================================\n")
+print("Seja bem vindo a hamburgueria do Ítalo Diniz")
+print("\n=========================================")
+print("           | Menu |\n")
+print("|Comida|   |Código|   |Valor|  ")
+print("Cachorro-quente = 100 = R$1,20\n")
+print("Bauru = 101 = R$1,30\n")
+print("Hambúrguer = 102 = R$1,20\n")
+print("Cheeseburguer = 103 = R$1,30\n")
+print("Refrigerante = 104 = R$1,00\n")
+pedido = int(input("Qual é seu pedido? --> "))
+if pedido == 100:
+    print("\nDeu o valor de R$1,20")
+elif pedido == 101:
+    print("\nDeu o valor de R$1,30")
+elif pedido == 102:
+    print("\nDeu o valor de R$1,20")
+elif pedido == 103:
+    print("\nDeu o valor de R$1,30")
+elif pedido == 104:
+    print("\nDeu o valor de R$1,00")
+else:
+    print("\nEste código não está registrado na tabela")
+

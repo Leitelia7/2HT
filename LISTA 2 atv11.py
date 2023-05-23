5b89705392ab3ad965e724a1c2c5190f4b393e09
Leitelia7 <134284637+Leitelia7@users.noreply.github.com>
2023-05-23 11:13:36 -0300

    Add files via upload

---
+++ LISTA 2 atv11.py
@@ -0,0 +1,14 @@
+q = float(input("Digite os quilometros rodados: --> "))
+l = float(input("Digite a quantidade do litros colocados: --> "))
+quilometros_rodados = q/l
+print("Os quilometros rodados do carro é: ", quilometros_rodados)
+
+
+
+
+
+
+
+
+
+
